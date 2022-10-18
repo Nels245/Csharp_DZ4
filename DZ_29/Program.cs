cc3feeded9e599bd8b53989e887d19aa9fbd6de6
Nels <boldirevpavel97@yandex.ru>
2022-10-18 22:13:41 +0300

    add DZ_29

---
+++ DZ_29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+
+int [] array = new int[8]; 
+FillArray(array);
+PrintArray(array);
+
+//Логика ------------------------------------------
+
+void FillArray(int [] array) 
+{
+    int i = 0;
+    int len = array.Length;
+    while(i < len)
+    {
+        array[i] = new Random().Next(0,10);
+        i++;    
+    }
+}
+
+void PrintArray(int [] array) //выводим массив
+{
+    int i = 0;
+    int len = array.Length;
+    while (i < len)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    }
+}
+
