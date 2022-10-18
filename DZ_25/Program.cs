b7b8225f131e028dfdf8d9659f163059af6432fe
Nels <boldirevpavel97@yandex.ru>
2022-10-18 21:40:13 +0300

    add DZ_25

---
+++ DZ_25/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход 
+//два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+int a = int.Parse(Console.ReadLine() ?? ""); //2
+int b = int.Parse(Console.ReadLine() ?? ""); //4
+
+int num = a;
+
+for( int i = 1; i < b; i++)
+{
+    num = num * a;
+}
+
+Console.Write($"{a}, {b} -> {num}");
